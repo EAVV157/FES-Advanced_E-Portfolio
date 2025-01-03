a4b80728a1ae8ef49f4f2a5a15625ef9d145ddfc
Edgar <edvargas1817@gmail.com>
2025-01-03 15:17:53 -0700

    Added Dark/Light mode functions. Also added personal animation (Matrix background)

--- index.js
+++ index.js
@@ -1,3 +1,61 @@
+let isModelOpen = true;
+let contrastToggle = false;
+
+/* *************** MATRIX BACKGROUND *************** */
+// Initialising the canvas
+var canvas = document.querySelector('canvas'),
+    ctx = canvas.getContext('2d');
+
+// Setting the width and height of the canvas
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Setting up the letters
+const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
+const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const nums = '0123456789';
+
+var letters = katakana + latin + nums;
+letters = letters.split('');
+
+// Setting up the columns
+var fontSize = 10,
+    columns = canvas.width / fontSize;
+
+// Setting up the drops
+var drops = [];
+for (var i = 0; i < columns; i++) {
+  drops[i] = 1;
+}
+
+// Setting up the draw function
+function draw() {
+  ctx.fillStyle = contrastToggle ? 'rgba(0, 0, 0, .1)' : 'rgba(255, 255, 255, .1)';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  for (var i = 0; i < drops.length; i++) {
+    var text = letters[Math.floor(Math.random() * letters.length)];
+    ctx.fillStyle = contrastToggle ? '#fff' : '#000';
+    ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+    drops[i]++;
+    if (drops[i] * fontSize > canvas.height && Math.random() > .95) {
+      drops[i] = 0;
+    }
+  }
+}
+
+// Loop the animation
+setInterval(draw, 33);
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if(contrastToggle) {
+    document.body.classList += " dark-theme";
+  }
+  else {
+    document.body.classList.remove("dark-theme");
+  }
+}
+
 function contact(event) {
   event.preventDefault();
   const loading = document.querySelector(".model__overlay--loading");
@@ -23,7 +81,6 @@ function contact(event) {
     });
 }
 
-let isModelOpen = true;
 function toggleModel() {
     isModelOpen = !isModelOpen;
     if(isModelOpen){
